83f0eed5ba44bedd584b94ba660bbee9d847ec1b
warnamannabandara <kavindi.bandara@gmail.com>
2026-01-04 23:18:06 +0530

    backend init

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Yohan!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
