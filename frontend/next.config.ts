b4716fd36e9906c279ab1b343c067d1e50ecd6ca
warnamannabandara <kavindi.bandara@gmail.com>
2026-03-28 11:34:42 +0530

    authentication

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  serverExternalPackages: ["mongoose"],
+  turbopack: {
+    root: "./",
+  },
+} as any;
 
 export default nextConfig;
