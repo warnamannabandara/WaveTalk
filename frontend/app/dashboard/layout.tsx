d9e07d4c0c31fe633ea54f8e379665c7c4f3b1fa
warnamannabandara <kavindi.bandara@gmail.com>
2026-04-27 11:50:33 +0530

    refactor: simplify DashboardLayout by removing Sidebar and using DashboardShell; delete unused DashboardPage

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,24 +1,5 @@
-import Sidebar from '../../components/dashboard/Sidebar';
+import DashboardShell from '../../components/dashboard/DashboardShell';
 
-export default function DashboardLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <div className="flex h-screen bg-[#15231D] text-white overflow-hidden font-sans">
-            {/* Sidebar - Fixed width */}
-            <aside className="w-64 flex-shrink-0">
-                <Sidebar />
-            </aside>
-
-            {/* Main Content Area */}
-            <main className="flex-1 flex flex-col min-w-0 overflow-hidden relative">
-                {/* Header could go here if global */}
-                <div className="flex-1 overflow-auto bg-[#15231D]">
-                    {children}
-                </div>
-            </main>
-        </div>
-    );
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    return <DashboardShell>{children}</DashboardShell>;
 }
